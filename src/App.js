82f52616be3c23cf835f654f9380672d21d7eefd
yaswanthbandaru <yaswanthbandaru424@gmail.com>
2024-02-07 12:05:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Yaswanth Bandaru
-        </a>
       </header>
     </div>
   );
